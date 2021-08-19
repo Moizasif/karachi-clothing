3aa0a17b381dc4f798f792e405534541cb8a60b4
Moizasif <kmoizasif019@gmail.com>
2021-08-19 20:20:20 +0500

    Added Header Component in our Application

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Route,Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/Shop';
+import Header from './components/header/Header';
 
 
 
@@ -10,6 +11,7 @@ function App() {
     //component we want to render that page
     //path is the URL
     <div>
+      <Header />
       <Switch>
       <Route exact path='/' component={HomePage} /> 
       <Route path='/shop' component={ShopPage} /> 
