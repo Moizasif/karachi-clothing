0b6a8d09899f799fcd3820677cbdb59f81603341
Moizasif <kmoizasif019@gmail.com>
2021-08-25 21:42:50 +0500

    Added Basic CartIcon Component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 import { ReactComponent as Logo} from '../../assets/crown.svg';
 import {connect} from 'react-redux'
+import CartIcon from '../cart-icon/CartIcon';
 import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss'
@@ -25,6 +26,7 @@ const Header = ({currentUser}) => {
                    :
                    <Link className="option" to="/signin">SIGN IN</Link>
                }
+               <CartIcon />
             </div>
         </div>
     )
