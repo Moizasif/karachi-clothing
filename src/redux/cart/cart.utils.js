d8573089523c479b23f0fca9e48a4552dbaaf32f
Moizasif <kmoizasif019@gmail.com>
2021-08-29 22:36:39 +0500

    Adding Multiple Items to Cart

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,18 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    //Checking cart Items already exist or not
+
+
+    const existingCartItem = cartItems.find(
+      cartItem => cartItem.id === cartItemToAdd.id
+    );
+  
+    if (existingCartItem) {
+      return cartItems.map(cartItem =>
+        cartItem.id === cartItemToAdd.id
+          ? { ...cartItem, quantity: cartItem.quantity + 1 }
+          : cartItem
+      );
+    }
+  
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  };
