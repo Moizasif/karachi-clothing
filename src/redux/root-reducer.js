06d15235d35871664b0bd2aa531a5fc40569dd09
Moizasif <kmoizasif019@gmail.com>
2021-09-28 23:15:36 +0500

    Directory state in to redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import storage from 'redux-persist/lib/storage'
 
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
+import directoryReducer from './directory/directory.reducer'
 
 
 //whitelist is an array which what we want to store
@@ -15,7 +16,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers ({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 }) 
 
 export default persistReducer(persistConfig, rootReducer)
